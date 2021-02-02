f324441ec3d0b3b83d86505f9ed646f1009f7da3
Bobby Del Bosque <bobbydelbosque@outlook.ocm>
2021-02-01 19:18:37 -0600

    update

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,4 +1,14 @@
-import os
 import csv
 
-Bank_csv = os.path.join(".."Resources","budget_data.csv")
+file_name = "Resources/election_data.csv"
+
+results_dict = {}
+total_votes = 0
+
+with open(file_name) as fh:
+    csv_reader = csv.reader(fh)
+
+    column_headers = next(csv_reader)
+
+    for row in csv_reader:
+       cur_candidate = row[2]
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -3,8 +3,4 @@ import csv
 file_name = "Resources/election_data.csv"
 
 with open(file_name) as fh:
-    csv_reader = csv.reader(fh)
-
-    for row in csv_reader:
-        print(row)
-        break
+    csv_reader = csv.reader(fh)    
